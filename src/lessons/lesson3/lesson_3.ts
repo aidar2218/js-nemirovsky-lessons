8577ecd22993ed8b20748537095b6da347c1cdc3
Aidar Ibraev <aidaribraev32@gmail.com>
2024-10-16 21:19:17 +0600

    Commit 3.2.7. Promises, create Promise, .then .catch .finally

--- src/lessons/lesson3/lesson_3.ts
+++ src/lessons/lesson3/lesson_3.ts
@@ -15,6 +15,40 @@ console.log('lesson 3');
 // https://jsonplaceholder.typicode.com/posts/1
 // https://habr.com/ru/company/oleg-bunin/blog/417461/?_ga=2.54695343.543933152.1602500664-1040035071.1596811661
 
+// let prom = new Promise((resolve, reject) => {
+//     setTimeout(response => {
+//         if (response.httpStatus >= 200 && response.httpStatus < 400) {
+//             resolve(response.data);
+//             console.log("2", prom);
+//         } else {
+//             reject(response.error);
+//         }
+//     }, 2000, {
+//         httpStatus: 200,
+//         data: {
+//             userName: "Yo",
+//             id: "sdfg234dfgj243234",
+//             status: "active"
+//         },
+//         error: {},
+//     });
+// });
+//
+// prom
+//     .then(
+//         res => {
+//             console.log("res", res);
+//         },
+//         () => {
+//         }
+//     )
+//     .then(res2 => {
+//         console.log("res2", res2);
+//     });
+//
+// console.log("1 ", prom);
+
 
 // just a plug
-export default ()=>{};
+export default () => {
+};
